3c673ec0b95fd339fe97b80ae8ba80f932027a09
vmueller13 <muellerveronica1@gmail.com>
2023-07-10 18:03:24 -0500

    updating files

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,30 +1,129 @@
 # Import the dependencies.
-
-
+import datetime as dt
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
 
 #################################################
 # Database Setup
 #################################################
+engine = create_engine("sqlite:///hawaii.sqlite")
+
 
 
 # reflect an existing database into a new model
-
+Base = automap_base()
 # reflect the tables
-
+Base.prepare(engine, reflect=True)
+Base.classes.keys()
 
 # Save references to each table
-
+Station = Base.classes.station
+Measurement = Base.classes.measurement
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
+#Create an instance of Flask
+app = Flask(__name__)
 
-
-
+# Define the homepage route
+@app.route("/")
+def home():
+    return (
+        f"Welcome to the Hawaii Climate Analysis API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/&lt;start&gt;<br/>"
+        f"/api/v1.0/&lt;start&gt;/&lt;end&gt;<br/>"
+    )
 
 #################################################
 # Flask Routes
 #################################################
+# Define the precipitation route
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Calculate the date 1 year ago from the most recent date in the database
+    one_year_ago = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+
+    # Query the precipitation data for the last 12 months
+    results = session.query(Measurement.date, Measurement.prcp) \
+                     .filter(Measurement.date >= one_year_ago) \
+                     .all()
+
+    # Create a dictionary with date as the key and prcp as the value
+    precipitation_data = {date: prcp for date, prcp in results}
+
+    return jsonify(precipitation_data)
+
+# Define the stations route
+@app.route("/api/v1.0/stations")
+def stations():
+    # Query the list of stations
+    results = session.query(Station.station).all()
+
+    # Convert the list of tuples to a list
+    station_list = list(np.ravel(results))
+
+    return jsonify(station_list)
+
+# Define the temperature observations route
+@app.route("/api/v1.0/tobs")
+def tobs():
+    # Calculate the date 1 year ago from the most recent date in the database
+    one_year_ago = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+
+   # Query the most active station
+    most_active_station = session.query(Measurement.station) \
+                                 .group_by(Measurement.station) \
+                                 .order_by(func.count(Measurement.station).desc()) \
+                                 .first()[0]
+
+    # Query the temperature observations for the last 12 months for the most active station
+    results = session.query(Measurement.date, Measurement.tobs) \
+                     .filter(Measurement.station == most_active_station) \
+                     .filter(Measurement.date >= one_year_ago) \
+                     .all()
+
+    # Create a list of temperature observations
+    temperature_list = list(np.ravel(results))
+
+    return jsonify(temperature_list)
+
+# Define the start and start-end range route
+@app.route("/api/v1.0/<start>")
+@app.route("/api/v1.0/<start>/<end>")
+def temperature_stats(start, end=None):
+    # Query the minimum, average, and maximum temperature for the specified date range
+    if end:
+        results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)) \
+                         .filter(Measurement.date >= start) \
+                         .filter(Measurement.date <= end) \
+                         .all()
+    else:
+        results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)) \
+                         .filter(Measurement.date >= start) \
+                         .all()
+
+    # Create a dictionary with temperature statistics
+    temperature_stats = {
+        'TMIN': results[0][0],
+        'TAVG': results[0][1],
+        'TMAX': results[0][2]
+    }
+
+    return jsonify(temperature_stats)
+
+# Run the Flask app
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ SurfsUp/wsgi.py
@@ -0,0 +1,11 @@
+# from app import app
+
+# if __name__ == "__main__":
+#     from waitress import serve
+#     serve(app, host='0.0.0.0', port=8000)
+
+
+from app import app
+
+if __name__ == "__main__":
+    app.run()
